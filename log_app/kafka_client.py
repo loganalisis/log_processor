583c715258afa67a7d83160efe120bd5c2e8f8ac
isubhamsr <shubhamroy12345@gmail.com>
2025-10-25 15:25:53 +0530

    add key ad send the log file data

--- log_app/kafka_client.py
+++ log_app/kafka_client.py
@@ -7,6 +7,6 @@ producer = KafkaProducer(
     value_serializer=lambda m: json.dumps(m).encode('utf-8')
 )
 
-def publish_to_kafka(topic, message):
-    producer.send(topic, message)
+def publish_to_kafka(topic, message, unique_name):
+    producer.send(topic, message, unique_name.encode('utf-8'))
     producer.flush()
--- log_app/views.py
+++ log_app/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .kafka_client import publish_to_kafka
 from azure.storage.blob import BlobClient
 import os, uuid
+import tempfile
 
 account_name = os.environ.get("AZURE_STORAGE_ACCOUNT_NAME")
 account_key = os.environ.get("AZURE_STORAGE_ACCOUNT_KEY")
@@ -25,14 +26,33 @@ def upload_log(request):
         blob_name=unique_name
     )
 
-    blob_client.upload_blob(file_obj, overwrite=True)
+    # blob_client.upload_blob(file_obj, overwrite=True)
     blob_url = f"https://{account_name}.blob.core.windows.net/{container_name}/{unique_name}"
+    
+
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        for chunk in file_obj.chunks():
+            temp_file.write(chunk)
+        temp_file_path = temp_file.name
+
+    with open(temp_file_path, 'r') as f:
+        for line_num, line in enumerate(f):
+            if line.strip():
+                msg = {
+                    "file_name": file_obj.name,
+                    "line_number": line_num + 1,
+                    "content": line.strip(),
+                    "blob_url": blob_url,
+                }
+                publish_to_kafka("logs_item2.uploaded", msg, unique_name)
+                # print(f"Publishing to Kafka: {msg}")
 
     # Send metadata to Kafka
-    publish_to_kafka("logs.uploaded", {"file": unique_name, "url": blob_url})
+    # publish_to_kafka("logs.uploaded", {"file": unique_name, "url": blob_url})
+    # publish_to_kafka("logs.uploaded", "hello")
 
     return Response({
         "message": "Uploaded to Azure Blob Storage",
-        "file": unique_name,
+        "unique_name": unique_name,
         "url": blob_url
     })
